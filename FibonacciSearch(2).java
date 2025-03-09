e982c77b50d705f484075967f50de615cb7de305
SHRUTI CHAKRABORTY <90564186+chakraborty-shruti@users.noreply.github.com>
2025-03-09 20:43:21 +0530

    FibonacciSearch(2).java

---
+++ FibonacciSearch(2).java
@@ -0,0 +1,23 @@
+//Fibonacci Using Tabulation(Bottom up)
+
+class Test{
+
+    static int nthFibonacci(int n){
+        if(n <= 1) return n;
+
+        //Table to store Fibonacci Values
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<= n; i++){
+            dp[i] = dp[i - 1]+ dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args){
+        System.out.println(nthFibonacci(5));
+    }
+}
