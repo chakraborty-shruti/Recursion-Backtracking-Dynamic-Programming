273696c8f7fbe60cfa5c5ebec987962efed44082
SHRUTI CHAKRABORTY <90564186+chakraborty-shruti@users.noreply.github.com>
2025-04-09 23:51:06 +0530

    NumberOfCoins.java

--- NumberOfCoins.java
+++ NumberOfCoins.java
@@ -1,3 +1,5 @@
+//Given an array of coins[] of size n and a target value sum, where coins[i] represent the coins of different denominations. You have an infinite supply of each of the coins. The task is to find the minimum number of coins required to make the given value sum. If it is not possible to form the sum using the given coins, return -1.
+
 import java.util.Arrays;
 
 class Test{
